86421c5de0d08994193c5353fbd941e6e0db58e5
Thales <thalesalves44@gmail.com>
2018-12-09 16:35:56 -0300

    criando a tela de produtos

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:loja_app/tabs/category_tab.dart';
 import 'package:loja_app/tabs/home_tab.dart';
 import 'package:loja_app/widgets/custom_drawer.dart';
 
@@ -14,7 +15,15 @@ class HomeScreen extends StatelessWidget {
       children: <Widget>[
         Scaffold(
           body: HomeTab(),
-          drawer: CustomDrawer(),
+          drawer: CustomDrawer(_pageController),
+        ),
+        Scaffold(
+          appBar: AppBar(
+            title: Text("Produtos"),
+            centerTitle: true,
+          ),
+          drawer: CustomDrawer(_pageController),
+          body: CategoryTab(),
         )
       ],
     );
---
+++ lib/tiles/category_tile.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class CategoryTile extends StatelessWidget {
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+
+    );
+  }
+}
