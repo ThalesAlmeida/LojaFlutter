8187dd70811732627bb09715d3869514f3d814c0
Thales <thalesalves44@gmail.com>
2018-12-15 21:19:49 -0300

    criando logica de adicionar no carrinho

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:loja_app/models/cart_model.dart';
 import 'package:loja_app/models/user_model.dart';
 import 'package:loja_app/screens/home_screen.dart';
-import 'package:loja_app/screens/login_screen.dart';
-import 'package:loja_app/screens/signup_screen.dart';
 import 'package:scoped_model/scoped_model.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+
   @override
   Widget build(BuildContext context) {
     return ScopedModel<UserModel>(
       model: UserModel(),
-      child: MaterialApp(
-        title: 'App',
-        theme: ThemeData(
-            primarySwatch: Colors.blue,
-            primaryColor: Color.fromARGB(255, 100, 4, 100)
-        ),
-        debugShowCheckedModeBanner: false,
-        home: HomeScreen(),
+      child: ScopedModelDescendant<UserModel>(
+          builder: (context, child, model){
+            return ScopedModel<CartModel>(
+              model: CartModel(model),
+              child: MaterialApp(
+                  title: "Flutter's Clothing",
+                  theme: ThemeData(
+                      primarySwatch: Colors.blue,
+                      primaryColor: Color.fromARGB(255, 4, 125, 141)
+                  ),
+                  debugShowCheckedModeBanner: false,
+                  home: HomeScreen()
+              ),
+            );
+          }
       ),
     );
   }
 }
-
