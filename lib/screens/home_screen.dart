460e6db8122be78987b4d536a736d15801c2f438
Thales <thalesalves44@gmail.com>
2018-12-15 21:49:37 -0300

    personalizando a tela do carrinho

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:loja_app/tabs/products_tab.dart';
 import 'package:loja_app/tabs/home_tab.dart';
+import 'package:loja_app/widgets/cart_button.dart';
 import 'package:loja_app/widgets/custom_drawer.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -16,6 +17,7 @@ class HomeScreen extends StatelessWidget {
         Scaffold(
           body: HomeTab(),
           drawer: CustomDrawer(_pageController),
+          floatingActionButton: CartButton(),
         ),
         Scaffold(
           appBar: AppBar(
@@ -23,6 +25,7 @@ class HomeScreen extends StatelessWidget {
             centerTitle: true,
           ),
           drawer: CustomDrawer(_pageController),
+          floatingActionButton: CartButton(),
           body: ProductsTab(),
         )
       ],
